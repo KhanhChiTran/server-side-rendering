f44cdfda3109954b48ae1d163ab4c7fb7b01a5b6
Chi Tran <khanhchi.tran13051@gmail.com>
2021-06-24 07:17:24 +0200

    Server side rendering

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const handlebars = require("express-handlebars");
+
+const PORT = 3000;
+app.use(express.static(path.join(__dirname, "public")));
+//Template engine
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs"
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resource/views"));
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+app.listen(PORT, () => console.log(`App listening at ${PORT}`));
